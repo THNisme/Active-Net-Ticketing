77c7bf63713a6f7e6b060f9ea584bd1a00a134a2
THNisme <nghiath.ce191115@gmail.com>
2025-10-08 21:25:03 +0700

    update(wysiwyg): can upload for demo

--- src/main/webapp/js/event-form-dashboard/custom-wysiwyg.js
+++ src/main/webapp/js/event-form-dashboard/custom-wysiwyg.js
@@ -8,9 +8,18 @@ const editor = new FroalaEditor("#eventDescription", {
   charCounterCount: true,
   placeholderText: 'Hãy viết mô tả cho sự kiện này...',
   quickInsertTags: ['h1', 'h2', 'h3', 'p'],
+  // imageUploadURL: '/upload-image',   // servlet xử lý upload ảnh - khi 
+  // videoUploadURL: '/upload-video',   // servlet xử lý upload video
+  imageUploadParam: 'file',          // tên param input (phải trùng servlet)
+  videoUploadParam: 'file',
+  imageAllowedTypes: ['jpeg', 'jpg', 'png', 'gif'],
+  videoAllowedTypes: ['mp4', 'webm', 'ogg'],
+
+  imageUpload: false,
+  imagePaste: true,
+  imageDefaultWidth: 300,
+  imageDefaultDisplay: 'block'
 });
 
-document.getElementById("show-content").addEventListener("click", () => {
-  const content = editor.html.get();
-  document.getElementById("eventDescription").value = content;
-});
+
+//Muốn upload ảnh thật qua servlet ===> Xóa imageUpload: false, giữ lại imageUploadURL và viết servlet
