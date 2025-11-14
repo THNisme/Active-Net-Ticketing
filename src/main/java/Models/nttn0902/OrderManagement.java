23eaea49ba73185c471b312efa6948fdd41e4b8a
NguyennttCA181645 <nguyennttca181645@fpt.edu.vn>
2025-11-14 19:29:12 +0700

    order management

---
+++ src/main/java/Models/nttn0902/OrderManagement.java
@@ -0,0 +1,195 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models.nttn0902;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.Date;
+
+/**
+ *
+ * @author NGUYEN
+ */
+public class OrderManagement {
+
+    private int orderId;
+    private String contactFullname;
+    private String contactEmail;
+    private String contactPhone;
+    private Timestamp orderDate;
+    private BigDecimal totalAmount;
+    private String eventName;
+    private Date createdAt;
+    private String categoryName;
+    private Timestamp startDate;
+    private Timestamp endDate;
+
+    private String placeName;
+    private String placeAddress;
+
+    private String serialNumber;
+    private String ticketTypeName;
+    private String zoneName;
+    private String rowLabel;
+    private String seatNumber;
+
+    public OrderManagement() {
+    }
+
+    public OrderManagement(int orderId, String contactFullname, String contactEmail,
+            String contactPhone, Timestamp orderDate, BigDecimal totalAmount) {
+        this.orderId = orderId;
+        this.contactFullname = contactFullname;
+        this.contactEmail = contactEmail;
+        this.contactPhone = contactPhone;
+        this.orderDate = orderDate;
+        this.totalAmount = totalAmount;
+    }
+
+    // Getters và Setters
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getContactFullname() {
+        return contactFullname;
+    }
+
+    public void setContactFullname(String contactFullname) {
+        this.contactFullname = contactFullname;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getContactPhone() {
+        return contactPhone;
+    }
+
+    public void setContactPhone(String contactPhone) {
+        this.contactPhone = contactPhone;
+    }
+
+    public Timestamp getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Timestamp orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public BigDecimal getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(BigDecimal totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Timestamp getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Timestamp startDate) {
+        this.startDate = startDate;
+    }
+
+    public Timestamp getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Timestamp endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getPlaceName() {
+        return placeName;
+    }
+
+    public void setPlaceName(String placeName) {
+        this.placeName = placeName;
+    }
+
+    public String getPlaceAddress() {
+        return placeAddress;
+    }
+
+    public void setPlaceAddress(String placeAddress) {
+        this.placeAddress = placeAddress;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getTicketTypeName() {
+        return ticketTypeName;
+    }
+
+    public void setTicketTypeName(String ticketTypeName) {
+        this.ticketTypeName = ticketTypeName;
+    }
+
+    public String getZoneName() {
+        return zoneName;
+    }
+
+    public void setZoneName(String zoneName) {
+        this.zoneName = zoneName;
+    }
+
+    public String getRowLabel() {
+        return rowLabel;
+    }
+
+    public void setRowLabel(String rowLabel) {
+        this.rowLabel = rowLabel;
+    }
+
+    public String getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(String seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+}
