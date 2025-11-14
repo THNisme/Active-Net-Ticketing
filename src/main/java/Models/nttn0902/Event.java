7d5c3c32af66d9ebe9f0aa670fbbc1fb52e43052
NguyennttCA181645 <nguyennttca181645@fpt.edu.vn>
2025-11-14 19:54:32 +0700

    order model

---
+++ src/main/java/Models/nttn0902/Event.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models.nttn0902;
+
+import java.util.Date;
+
+/**
+ *
+ * @author NGUYEN
+ */
+public class Event {
+    private int eventID;
+    private String eventName;
+    private Date startDate;
+    private Date endDate;
+    private String imageURL;
+
+    public int getEventID() {
+        return eventID;
+    }
+
+    public void setEventID(int eventID) {
+        this.eventID = eventID;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+}
