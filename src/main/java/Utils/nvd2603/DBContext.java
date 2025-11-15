768d9324ba7dcb1b53105e49a8e7381de969edec
THNisme <nghiath.ce191115@gmail.com>
2025-11-15 10:53:45 +0700

    Update pom.xml - Comment Utils/DBContext.java & /nvd2603/DBContext.java - Fixbug login.jsp

--- src/main/java/Utils/nvd2603/DBContext.java
+++ src/main/java/Utils/nvd2603/DBContext.java
@@ -14,38 +14,38 @@ import java.util.logging.Logger;
  *
  * @author Nguyen Viet Duc
  */
-public class DBContext {
-
-    protected Connection conn;
-
-    public DBContext() {
-        try {
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            String dbURL = "jdbc:sqlserver://localhost:1433;"
-                    + "databaseName=Active-Net-Ticketing;"
-//                    + "databaseName=SWP391;"
-                    + "user=sa;"
-                    + "password=123456;"
-                    + "encrypt=true;trustServerCertificate=true;";
-            conn = DriverManager.getConnection(dbURL);
-            if (conn != null) {
-                DatabaseMetaData dm = conn.getMetaData();
-                System.out.println("✅ Kết nối DB thành công!");
-                System.out.println("Driver name: " + dm.getDriverName());
-                System.out.println("Driver version: " + dm.getDriverVersion());
-                System.out.println("Product name: " + dm.getDatabaseProductName());
-                System.out.println("Product version: " + dm.getDatabaseProductVersion());
-            }
-        } catch (SQLException ex) {
-            System.out.println("❌ Lỗi kết nối DB: " + ex.getMessage());
-            ex.printStackTrace();
-        } catch (ClassNotFoundException ex) {
-            System.out.println("❌ Không tìm thấy driver SQL Server!");
-            ex.printStackTrace();
-        }
-    }
-
-    public Connection getConnection() {
-        return conn;
-    }
-}
+//public class DBContext {
+//
+//    protected Connection conn;
+//
+//    public DBContext() {
+//        try {
+//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//            String dbURL = "jdbc:sqlserver://localhost:1433;"
+//                    + "databaseName=Active-Net-Ticketing;"
+////                    + "databaseName=SWP391;"
+//                    + "user=sa;"
+//                    + "password=123456;"
+//                    + "encrypt=true;trustServerCertificate=true;";
+//            conn = DriverManager.getConnection(dbURL);
+//            if (conn != null) {
+//                DatabaseMetaData dm = conn.getMetaData();
+//                System.out.println("✅ Kết nối DB thành công!");
+//                System.out.println("Driver name: " + dm.getDriverName());
+//                System.out.println("Driver version: " + dm.getDriverVersion());
+//                System.out.println("Product name: " + dm.getDatabaseProductName());
+//                System.out.println("Product version: " + dm.getDatabaseProductVersion());
+//            }
+//        } catch (SQLException ex) {
+//            System.out.println("❌ Lỗi kết nối DB: " + ex.getMessage());
+//            ex.printStackTrace();
+//        } catch (ClassNotFoundException ex) {
+//            System.out.println("❌ Không tìm thấy driver SQL Server!");
+//            ex.printStackTrace();
+//        }
+//    }
+//
+//    public Connection getConnection() {
+//        return conn;
+//    }
+//}
