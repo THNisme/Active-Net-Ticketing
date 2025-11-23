ec74c872f62e51166429508161431f97df684301
KietTT <kiettt.ce190871@gmail.com>
2025-11-23 18:29:47 +0700

    fix models

--- src/main/java/Models/ttk2008/Wallet.java
+++ src/main/java/Models/ttk2008/Wallet.java
@@ -4,21 +4,52 @@ import java.math.BigDecimal;
 import java.sql.Timestamp;
 
 public class Wallet {
+
     private int walletID;
     private int userID;
     private BigDecimal balance;
     private Timestamp lastUpdated;
+    private long promotionAmount;
 
     // Getters & Setters
-    public int getWalletID() { return walletID; }
-    public void setWalletID(int walletID) { this.walletID = walletID; }
+    public int getWalletID() {
+        return walletID;
+    }
 
-    public int getUserID() { return userID; }
-    public void setUserID(int userID) { this.userID = userID; }
+    public void setWalletID(int walletID) {
+        this.walletID = walletID;
+    }
 
-    public BigDecimal getBalance() { return balance; }
-    public void setBalance(BigDecimal balance) { this.balance = balance; }
+    public int getUserID() {
+        return userID;
+    }
 
-    public Timestamp getLastUpdated() { return lastUpdated; }
-    public void setLastUpdated(Timestamp lastUpdated) { this.lastUpdated = lastUpdated; }
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public Timestamp getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Timestamp lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public long getPromotionAmount() {
+        return promotionAmount;
+    }
+
+    public void setPromotionAmount(long promotionAmount) {
+        this.promotionAmount = promotionAmount;
+    }
+
 }
